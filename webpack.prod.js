f117cc91f83b7ec0fa42e6a2171c2c1e65aa8886
Zain Mustafa <zz.mhmail@gmail.com>
2023-07-19 19:10:12 -0400

    Set up configs for dev and prod'

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,24 @@
 const path = require('path');
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
 module.exports = {
+    entry: './src/index.js',
+    mode: "production",
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new MiniCssExtractPlugin()
+    ],
     output: {
         filename: 'main.[contenthash].js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['css-loader']
+            }
+        ]
     }
 }
